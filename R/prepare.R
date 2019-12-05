6b31b9ea4a67734b0c74bd5aa3d4ed698a348a10
frhl <frederik.heymann@gmail.com>
2019-12-05 17:07:54 -0500

    updated description and test corrcet git user

--- R/prepare.R
+++ R/prepare.R
@@ -62,10 +62,10 @@ prepare <- function(bait, infile, cols = NULL, impute = NULL){
 }
 
 # need to do it bait-wise instead of file-wise
-bait = unlist(strsplit(c('SMC_EDNRA','SMC_FLT','EC_FLT1')[2],'\\_')) #,'EC_JCAD')
-infile = list.files('~/Desktop/MICOM/WhiteheadData/', full.names = T)[2]
-dat = prepare(bait, infile, cols = c("Accession", "Intensity.SMC_FLT1.iTRAQ4.114.", "Intensity.SMC_mockF1.iTRAQ4.116.", 
-                                            "Intensity.SMC_FLT2.iTRAQ4.115.", "Intensity.SMC_mockF2.iTRAQ4.117."))
+#bait = unlist(strsplit(c('SMC_EDNRA','SMC_FLT','EC_FLT1')[2],'\\_')) #,'EC_JCAD')
+#infile = list.files('~/Desktop/MICOM/WhiteheadData/', full.names = T)[2]
+#dat = prepare(bait, infile, cols = c("Accession", "Intensity.SMC_FLT1.iTRAQ4.114.", "Intensity.SMC_mockF1.iTRAQ4.116.", 
+#                                            "Intensity.SMC_FLT2.iTRAQ4.115.", "Intensity.SMC_mockF2.iTRAQ4.117."))
 
 # 0.45441764  0.10679001  0.89405064 -0.04431094  0.58848592 -0.10070931
 
